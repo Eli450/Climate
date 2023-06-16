804b6e52e204588ee11e7e11cbbe371e4f42c703
Eliane Sebastião <elianebelita450@gmail.com>
2023-06-16 23:04:30 +0100

    Alterar o design do estado inicial da página

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import Condition from '@/app/components/Condition'
 
 async function getWeather(city){
-  const response = await fetch (`http://api.weatherapi.com/v1/current.json?key=a4d22e340b0f41ed996184640231505&q=${city}&aqi=no`)
+  const response = await fetch (`https://api.weatherapi.com/v1/current.json?key=a4d22e340b0f41ed996184640231505&q=${city}&aqi=no`)
 
   if (response.status === 200){
      const {current, location} = await response.json()
@@ -36,15 +36,21 @@ export default function Home() {
     return (
     <main className="flex w-full min-h-screen flex-col items-center justify-between p-24">
           <div className='w-1/2 h-max bg-white rounded-md flex justify-center p-16 flex-col' >
-              <div className={`${poppinsRegular.className}`}>
-                <span className={`block text-xl text-center font-medium ${poppinsBlack.className}`}>{cityAndCountry}</span>
-                <span className='block pt-3 text-center text-gray-300'>{time}</span>
-              </div>
-              <div>
-                <Condition condition={condition}></Condition>
-                <span className={`block text-6xl text-center font-lg pt-8 ${poppinsBlack.className}`}>{temp}ºC</span>
-                <span className='text-gray-300 text-center block'>{condition}</span>
-              </div>
+              { condition !== '' ? (<>
+                <div className={`${poppinsRegular.className}`}>
+                      <span className={`block text-xl text-center font-medium ${poppinsBlack.className}`}>{cityAndCountry}</span>
+                      <span className='block pt-3 text-center text-gray-300'>{time}</span>
+                    </div>
+                <div>
+                      <Condition condition={condition}></Condition>
+                      <span className={`block text-6xl text-center font-lg pt-8 ${poppinsBlack.className}`}>{temp}ºC</span>
+                      <span className='text-gray-300 text-center block'>{condition}</span>
+                    </div>
+              </>) : (<>
+                <div>
+                  <p className='text-center pt-16'>Digite uma cidade ou país</p>
+                </div>
+              </>) }
               <div className={`flex justify-center pt-24`}>
                   <form>
                       <input value={city} onChange={(e) =>{setCity(e.target.value)}} className={`h-10 w-40 rounded-l-lg bg-neutral-100 p-4 focus:outline-gray-300`}type='text' name='' placeholder='Digite a Cidade' />
